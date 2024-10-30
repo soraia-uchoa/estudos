e7d29486c6ade32abf917926599744c113a29c6f
soraia-uchoa <soraiavercosa3@icloud.com>
2024-10-30 13:07:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 alert('bem-vindo a soneca!');
 
-let name = prompt('qual seu nome?')
-let sleep = prompt('você gosta de dormir?')
-let choice = prompt('vc quer dormir aonde? (rede/cama/chão)?')
+let name = prompt('qual seu nome?');
+let sleep = prompt('você gosta de dormir?');
+let choice = prompt('vc quer dormir aonde? (rede/cama/chão)?')/
 
-alert('resumo das escolhas')
+alert('resumo das escolhas');
 
 alert('obrigado, ' + name + '!');
