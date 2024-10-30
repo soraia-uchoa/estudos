12b1b819f6660e56f242d4a37563a9922f9b6581
soraia-uchoa <soraiavercosa3@icloud.com>
2024-10-30 15:21:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ let name = prompt('qual seu nome?');
 let sleep = prompt('você gosta de dormir?');
 let choice = prompt('vc quer dormir aonde? (rede/cama/chão)?');
 
-alert('resumo das escolhas');
-
 alert('obrigado, ' + name + '!');
