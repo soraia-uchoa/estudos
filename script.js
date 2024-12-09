bbf3e7c67c13b02659ffb694babd64d4c3530038
soraia-uchoa <soraiavercosa3@icloud.com>
2024-12-09 16:42:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ let choice = prompt('vc quer dormir aonde? (rede/cama/chão)?');
 
 alert('obrigado, ' + name + '!');
 
-alert('meu doce favorito é jujuba');
-
-let sabor = prompt('qual meu sabor favorito?');
-
-alert('o que eu gosto de fazer nas horas vagas');
-
-let option = prompt('qual eu mais gosto? (dormir/assistir/jogar/comer)?');
-
-alert('obrigado, ' + name + '!');
