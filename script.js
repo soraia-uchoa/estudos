58bc531bde0aaaff6eacd6d42313f46acf11ef42
soraia <soraiavercosa3@icloud.com>
2025-03-12 14:22:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ if (password === passwordConfirmation) {
 const button = document.getElementById('register-button')
 button.addEventListener('click' , register)
 
+//////////////////////////////////////////////////////////////////
+
+//variavel
+const whiteGray = '#f1f5f9'
+const blackGray = '#212529'
+
+//função de manipulação de estilos
+function useLighTheme() {
+    document.body.style.backgroundColor = whiteGray
+    document.body.style.color = blackGray 
+
+}
+
+function userDarkTheme() {
+    document.body.style.backgroundColor = blackGray
+    document.body.style.color = whiteGray
+}
+
+//adicionar os eventos
+document.getElementById('lightBtn').addEventListener('click', useLighTheme)
+document.getElementById('darkBtn').addEventListener('click', userDarkTheme)
+
+
+
+
